60f89b37cfd5fc92315fa170e08eb6187bca8956
alex veith <aveith@bu.edu>
2019-04-20 16:47:08 -0400

    userinputpush

--- app/src/main/java/com/example/mrkrabs/MainActivity.java
+++ app/src/main/java/com/example/mrkrabs/MainActivity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
+
+
+    private Button enterbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterbutton = (Button) findViewById(R.id.enter);
     }
 }
