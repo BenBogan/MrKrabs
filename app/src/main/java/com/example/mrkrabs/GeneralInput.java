655769806ef06914681e9feb080706a62253b575
alex veith <aveith@bu.edu>
2019-04-24 19:43:54 -0400

    userinputpush

--- app/src/main/java/com/example/mrkrabs/GeneralInput.java
+++ app/src/main/java/com/example/mrkrabs/GeneralInput.java
@@ -7,11 +7,13 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class GeneralInput extends Activity implements OnClickListener{
 
 
     private Button GeneralButton;
+    private EditText MonthlyRent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
