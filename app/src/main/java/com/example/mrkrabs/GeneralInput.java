11f09fccf8b7726dafcaeaff45436131d5b636fa
alex veith <aveith@bu.edu>
2019-04-27 11:24:57 -0400

    userinputpush

--- app/src/main/java/com/example/mrkrabs/GeneralInput.java
+++ app/src/main/java/com/example/mrkrabs/GeneralInput.java
@@ -57,7 +57,7 @@ public class GeneralInput extends Activity implements OnClickListener{
         String City2 = CitiesList2.getText().toString();
         String Salary= ExpectedSalary.getText().toString();
 
-        if ((!Arrays.asList(Cities).contains(City1))&&(!Arrays.asList(Cities).contains(City2))) {
+        if ((!(Arrays.asList(Cities).contains(City1)))&&(!(Arrays.asList(Cities).contains(City2)))) {
             Toast CurrentError = Toast.makeText(GeneralInput.this, "Please Enter A Supported Current City", Toast.LENGTH_LONG);
             CurrentError.setGravity(Gravity.CENTER, 0, -1500);
             Toast DestinationError = Toast.makeText(GeneralInput.this, "Please Enter A Supported Destination City", Toast.LENGTH_LONG);
