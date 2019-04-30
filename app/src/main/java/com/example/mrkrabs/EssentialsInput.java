cf143d019d1de4db5a5ed714cf65ca990edd6318
RobertoLF <44534095+RobertoLF@users.noreply.github.com>
2019-04-29 21:47:27 -0400

    Merge remote-tracking branch 'origin/Roberto' into Titties
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/mrkrabs/EssentialsInput.java
+++ app/src/main/java/com/example/mrkrabs/EssentialsInput.java
@@ -26,7 +26,7 @@ public class EssentialsInput extends Activity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.essentials_input);
 
-        String[] TransportOptions = new String[]{"Public", "Personal"};
+        String[] TransportOptions = new String[]{"Select","Public", "Personal"};
 
         ArrayAdapter<String> TransportAdapter = new ArrayAdapter<String>(this,
                 R.layout.dropdown_layout, TransportOptions);
